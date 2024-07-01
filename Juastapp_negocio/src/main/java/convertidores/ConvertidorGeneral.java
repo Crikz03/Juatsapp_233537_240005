d5d177db83954368016f69fcec4c64ab9dcd3a7d
Lunatikera <carlitosjuven@gmail.com>
2024-07-01 05:52:30 -0700

    push

--- Juastapp_negocio/src/main/java/convertidores/ConvertidorGeneral.java
+++ Juastapp_negocio/src/main/java/convertidores/ConvertidorGeneral.java
@@ -4,30 +4,67 @@
  */
 package convertidores;
 
+import java.lang.reflect.Field;
+
 /**
  *
  * @author eljulls
  */
 public class ConvertidorGeneral {
-    public static <E, D> D convertEntityToDTO(E entity, Class<D> dtoClass) {
-        D dto = null;
+
+    // Método genérico para convertir un DTO a una entidad (Mismos Nombres)
+    public static <D, E> E convertidorEntidad(D dto, Class<E> entityClass) {
         try {
-            dto = dtoClass.newInstance();
-          //  BeanUtils.copyProperties(entity, dto);
-        } catch (InstantiationException | IllegalAccessException e) {
-            // manejar excepción
+            E entidad = entityClass.getDeclaredConstructor().newInstance();
+
+            // Obtener los campos del DTO y de la entidad
+            Field[] camposDto = dto.getClass().getDeclaredFields();
+            Field[] camposEntidad = entityClass.getDeclaredFields();
+
+            // Iterar sobre los campos y copiar los valores
+            for (Field campoDto : camposDto) {
+                for (Field campoEntidad : camposEntidad) {
+                    if (campoDto.getName().equals(campoEntidad.getName())
+                            && campoDto.getType().equals(campoEntidad.getType())) {
+                        campoDto.setAccessible(true);
+                        campoEntidad.setAccessible(true);
+                        campoEntidad.set(entidad, campoDto.get(dto));
+                        break;
+                    }
+                }
+            }
+
+            return entidad;
+        } catch (Exception e) {
+            throw new RuntimeException("Error al convertir DTO a entidad", e);
         }
-        return dto;
     }
 
-    public static <E, D> E convertDTOToEntity(D dto, Class<E> entityClass) {
-        E entity = null;
+    // Método genérico para convertir una Entidad a un DTO (Mismos Nombres)
+    public static <E, D> D convertidoraDTO(E entidad, Class<D> claseDto) {
         try {
-            entity = entityClass.newInstance();
-         //   BeanUtils.copyProperties(dto, entity);
-        } catch (InstantiationException | IllegalAccessException e) {
-            // manejar excepción
+            D dto = claseDto.getDeclaredConstructor().newInstance();
+
+            // Obtener los campos de la entidad y del DTO
+            Field[] camposEntidad = entidad.getClass().getDeclaredFields();
+            Field[] camposDto = claseDto.getDeclaredFields();
+
+            // Iterar sobre los campos y copiar los valores
+            for (Field campoEntidad : camposEntidad) {
+                for (Field campoDto : camposDto) {
+                    if (campoEntidad.getName().equals(campoDto.getName())
+                            && campoEntidad.getType().equals(campoDto.getType())) {
+                        campoEntidad.setAccessible(true);
+                        campoDto.setAccessible(true);
+                        campoDto.set(dto, campoEntidad.get(entidad));
+                        break;
+                    }
+                }
+            }
+
+            return dto;
+        } catch (Exception e) {
+            throw new RuntimeException("Error al convertir entidad a DTO", e);
         }
-        return entity;
     }
 }
